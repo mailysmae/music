85f32d233d8bca56c2dec6bd727302ade8a54263
mailysmae <79702008+mailysmae@users.noreply.github.com>
2022-11-12 00:19:42 +0100

    FEAT: page accueil css and js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
+"use strict";
 const box = document.querySelector('.box');
 let scrollLine =  document.querySelector('.scroll');
 
@@ -5,4 +6,17 @@ box.addEventListener('wheel', (e)=> {
       e.preventDefault();
       box.scrollLeft += e.deltaY;
       scrollLinr.style.width = box.scrollLeft / 2 + 'px';
-})
+});
+
+let burger = document.querySelector(".bouton-menu");
+burger.addEventListener("click", toggle);
+
+function toggle(){
+    if(document.body.hasAttribute("burger-menu")){
+        document.body.removeAttribute("burger-menu");
+        burger.innerHTML = "Menu";
+    }else{
+        document.body.setAttribute("burger-menu", true);
+        burger.innerHTML = "Retour";
+    }
+};
