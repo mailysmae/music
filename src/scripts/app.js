6a2669d6d75ee6edc51e7c4ad5c375ef519c28c2
mailysmae <79702008+mailysmae@users.noreply.github.com>
2022-11-13 12:35:54 +0100

    FEAT: test player

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,22 +1,21 @@
 "use strict";
-const box = document.querySelector('.box');
-let scrollLine =  document.querySelector('.scroll');
-
-box.addEventListener('wheel', (e)=> {
-      e.preventDefault();
-      box.scrollLeft += e.deltaY;
-      scrollLinr.style.width = box.scrollLeft / 2 + 'px';
-});
-
 let burger = document.querySelector(".bouton-menu");
 burger.addEventListener("click", toggle);
 
 function toggle(){
     if(document.body.hasAttribute("burger-menu")){
         document.body.removeAttribute("burger-menu");
-        burger.innerHTML = "Menu";
+        //burger.innerHTML = "Menu";
     }else{
         document.body.setAttribute("burger-menu", true);
-        burger.innerHTML = "Retour";
+        //burger.innerHTML = "Retour";
     }
+};
+
+var audio = document.getElementById("audio");
+var play = document.getElementById("play");
+
+play.onClick = function(){
+  audio.play();
+  console.log ("play");
 };
